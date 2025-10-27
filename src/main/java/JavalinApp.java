b06109f2fc2361fd31d8a90c88138386309f12c8
Markus Spiekermann <markus.spiekermann@huawei.com>
2025-10-27 15:15:38 +0000

    init

---
+++ src/main/java/JavalinApp.java
@@ -0,0 +1,11 @@
+import io.javalin.Javalin;
+
+public class JavalinApp {
+
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
+
