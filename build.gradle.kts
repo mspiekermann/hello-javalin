f7a9bea00a273fdfe0fecd1a9ddf343bde029e17
Markus Spiekermann <markus.spiekermann@huawei.com>
2025-10-29 07:44:47 +0000

    fix Optional handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,17 @@ repositories {
     mavenCentral()
 }
 
+
+
+val jacksonVersion = "2.17.2"
+
 dependencies {
     implementation("io.javalin:javalin:6.7.0")
     implementation("org.slf4j:slf4j-simple:2.0.16")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:${jacksonVersion}")
+
 }
 
 application {
--- src/main/java/JavalinApp.java
+++ src/main/java/JavalinApp.java
@@ -1,24 +1,19 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.javalin.Javalin;
 import io.javalin.json.JavalinJackson;
-import io.javalin.json.JavalinJacksonKt;
 import user.UserController;
 
 public class JavalinApp {
 
     public static void main(String[] args) {
 
-        JavalinJackson.defaultMapper().registerModule(new Jdk8Module()); // <-- handles java.util.Optional
-
-        var app = Javalin.create(/*config*/)
+        var app = Javalin.create(cfg -> cfg.jsonMapper(new JavalinJackson().updateMapper(mapper -> mapper.registerModule(new Jdk8Module()))))
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
 
         app.get("/users", UserController.fetchAllUsernames);
         app.get("/users/{id}", UserController.fetchById);
     }
-
 
 }
 
